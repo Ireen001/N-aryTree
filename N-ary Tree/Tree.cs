6c977fe72f2f89d4d59ab13ba08f055fd8c725df
Ireen Heemels <ireen.heemels@hotmail.com>
2019-01-16 12:00:09 +0100

    Add project files.

---
+++ N-ary Tree/Tree.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_ary_Tree
+{
+    public class Tree<T>
+    {
+        public int Count { get; set; }
+
+        public int LeafCount { get; set;}
+
+        public List<TreeNode<T>> TheTree = new List<TreeNode<T>>();
+
+        public Tree()
+        {
+            Count = 0;
+            LeafCount = 0;
+        }
+
+        public TreeNode<T> AddChildNode(TreeNode<T> parentNode, T Value)
+        {
+            Count++;
+            TreeNode<T> newNode = new TreeNode<T>(Value, parentNode);
+            if (parentNode != null)
+            {
+                TheTree.Add(newNode);
+                parentNode.Child.Add(newNode);
+            }
+            else
+            {
+                TheTree.Add(newNode);
+
+            }
+            LeafCount = 0;
+            foreach (TreeNode<T> leaf in TheTree)
+            {
+                if (leaf.Child.Count == 0)
+                {
+                    LeafCount++;
+                }
+            }
+            return newNode;
+        }
+     public void removeNode(TreeNode<T> node)
+        {
+            this.TheTree.Remove(node); // node uit tree halen
+            Count--;
+            var parentNode = node.Parent; // uit node zijn parent halen
+            parentNode.Child.Remove(node); // haal node uit de parent
+
+            if(node.Child.Count == 0)
+            { }
+            else
+            {
+                for (int i = node.Child.Count-1; i>=0; i--)
+                {
+                    removeNode(node.Child[i]);
+                }
+            }
+            LeafCount = 0;
+            foreach (TreeNode<T> leaf in TheTree)
+            {
+                if (leaf.Child.Count == 0)
+                {
+                    LeafCount++;
+                }
+            }
+        }
+        public List<T> TraverseNodes()
+        {
+            List<T> traversednodes = new List<T>();
+
+            foreach (TreeNode<T> node in TheTree)
+            {
+                traversednodes.Add(node.Value);
+            }
+            return traversednodes;
+
+        }
+
+        public List<T> SumToLeafs()
+        {
+            List<T> AllSom = new List<T>();
+            List<TreeNode<T>> AllLeafs = new List<TreeNode<T>>();
+
+            foreach(TreeNode<T> leaf in AllLeafs)
+            {
+                
+                TreeNode<T> Parent = leaf;
+                dynamic Som = 0;
+                Som = Som + Parent.Value;
+                while(Parent.Parent != null)
+                {
+                    Som = Som + Parent.Parent.Value;
+                    Parent = Parent.Parent;
+                }
+                AllSom.Add(Som);
+            }
+            return AllSom;
+        }
+
+    }
+}
